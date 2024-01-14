c40a6c896b7d57b6b5a568557faa223f6a393099
Tinodaishe Tembo <synthmancer@gmail.com>
2024-01-14 16:17:35 +0200

    small refactor

--- src/Celeste.ts
+++ src/Celeste.ts
@@ -1,9 +1,16 @@
+// Model Stuff
+
 interface BitProps {
 	name?: string;
 	age?: number;
 }
 
+type Callback = () => {};
+
+// Sort of Components in your Favorite Frameworks
 export class Bit {
+	events: { [key: string]: Callback[] } = {};
+
 	constructor(private data: BitProps) {}
 
 	get(propName: string): number | string {
@@ -13,4 +20,7 @@ export class Bit {
 	set(update: BitProps): void {
 		Object.assign(this.data, update);
 	}
+
+	// EventHandler
+	on(eventName: string, callback: Callback) {}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { Bit } from "./Celeste/Models/Bit";
+import { Bit } from "./Celeste";
 
+// Dummy User Functionality, sort of like a component in React Frameworks
 const bit = new Bit({ name: "myname", age: 45 });
 
 bit.set({ name: "bro" });
 
 console.log(bit.get("name"));
-console.log(bit.get("age"));
