bc663c506a2939e4024fb94d778bf62a8a8f2ae8
Tinodaishe Tembo <synthmancer@gmail.com>
2024-01-18 14:54:19 +0200

    wip

--- src/lib/celeste.ts
+++ src/lib/celeste.ts
@@ -1,6 +1,8 @@
 import { Eventing } from "./core/event";
 import { Sync } from "./core/sync";
+import { Attributes } from "./core/attributes";
 
+// Demo
 export interface CelesteProps {
 	id?: number;
 	name?: string;
@@ -12,14 +14,9 @@ const rootUrl = "http://localhost:4000/bits";
 export class Celeste {
 	public events: Eventing = new Eventing();
 	public sync: Sync<CelesteProps> = new Sync<CelesteProps>(rootUrl);
+	public attributes: Attributes<CelesteProps>;
 
-	constructor(private data: CelesteProps) {}
-
-	get(propName: string): number | string {
-		return this.data[propName];
-	}
-
-	set(update: CelesteProps): void {
-		Object.assign(this.data, update);
+	constructor(attrs: CelesteProps) {
+		this.attributes = new Attributes<CelesteProps>(attrs);
 	}
 }
