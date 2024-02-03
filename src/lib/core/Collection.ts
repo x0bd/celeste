3c47701b1aa0537bedbeac4c9e7432f6f15ee840
Tinodaishe Tembo <synthmancer@gmail.com>
2024-02-03 17:53:24 +0000

    wip

--- src/lib/core/Collection.ts
+++ src/lib/core/Collection.ts
@@ -2,26 +2,26 @@ import { Eventing } from "./Event";
 import axios, { AxiosResponse } from "axios";
 
 export class Collection<T, K> {
-	models: T[] = [];
-	events: Eventing = new Eventing();
+  models: T[] = [];
+  events: Eventing = new Eventing();
 
-	constructor(public rootUrl: string, public deserialize: (json: K) => T) {}
+  constructor(public rootUrl: string, public deserialize: (json: K) => T) {}
 
-	get on() {
-		return this.events.on;
-	}
+  get on() {
+    return this.events.on;
+  }
 
-	get trigger() {
-		return this.events.trigger;
-	}
+  get trigger() {
+    return this.events.trigger;
+  }
 
-	fetch(): void {
-		axios.get(this.rootUrl).then((response: AxiosResponse) => {
-			response.data.forEach((value: K) => {
-				this.models.push(this.deserialize(value));
-			});
+  fetch(): void {
+    axios.get(this.rootUrl).then((response: AxiosResponse) => {
+      response.data.forEach((value: K) => {
+        this.models.push(this.deserialize(value));
+      });
 
-			this.trigger("change");
-		});
-	}
+      this.trigger("change");
+    });
+  }
 }
