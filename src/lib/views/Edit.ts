82d6bf8580da81a83bc42505e3f27067ebb21772
Tinodaishe Tembo <synthmancer@gmail.com>
2024-02-07 18:06:48 +0200

    wip

--- src/lib/views/Edit.ts
+++ src/lib/views/Edit.ts
@@ -1,5 +1,7 @@
 import { View } from "./View";
 import { User, CelesteProps } from "../core/User";
+import { Form } from "./Form";
+import { Show } from "./Show";
 
 export class Edit extends View<User, CelesteProps> {
 	regionsMap(): { [key: string]: string } {
@@ -7,6 +9,11 @@ export class Edit extends View<User, CelesteProps> {
 			show: ".user-show",
 			form: ".user-form",
 		};
+	}
+
+	onRender(): void {
+		new Show(this.regions.show, this.model).render();
+		new Form(this.regions.form, this.model).render();
 	}
 
 	template(): string {
--- src/lib/views/View.ts
+++ src/lib/views/View.ts
@@ -48,13 +48,19 @@ export abstract class View<T extends Celeste<K>, K> {
 		}
 	}
 
+	onRender(): void {}
+
 	render(): void {
 		this.parent.innerHTML = "";
+
 		const templateElement = document.createElement("template");
 		templateElement.innerHTML = this.template();
 
 		this.bindEvents(templateElement.content);
 		this.mapRegions(templateElement.content);
+
+		this.onRender();
+
 		this.parent.append(templateElement.content);
 	}
 }
